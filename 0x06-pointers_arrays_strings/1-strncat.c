5712933b8d89647395fc49220d84ab7f419776ef
moughtari7 <Moughtari7fx@gmail.com>
2022-07-14 20:41:23 +0300

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,25 @@
-#include "holberton.h"
+#include "main.h"
+#include <string.h>
+
 /**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
-*/
+ * _strncat -  function that concatenates two strings.
+ * @src: cadena a copiar
+ * @dest: destino de la cadena.
+ * @n: number.
+ * Return: dest.
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int index = strlen(dest);
+	int a = 0;
 
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-;
-
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
-
-/*should end with a end of string char*/
-dest[dest_len + i] = '\0';
-
-return (dest);
+	while (a < n && *src)
+	{
+		dest[index + a] = *src;
+		src++;
+		a++;
+	}
+	dest[index + a] = '\0';
+	return (dest);
 }
