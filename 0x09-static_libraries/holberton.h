a94dc1cadb798fba0fd294be665ccc558549f640
moughtari7 <Moughtari7fx@gmail.com>
2022-07-23 00:43:56 +0300

    Update holberton.h

--- 0x09-static_libraries/holberton.h
+++ 0x09-static_libraries/holberton.h
@@ -1,7 +1,5 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
-/*copy list from task*/
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _HOLBERTON_H_ */
